4c91e286607b67e23e56293695be911388c54a47
Abhishek Soni <abhishek1600soni@gmail.com>
2023-05-17 22:58:47 +0530

    Made GitHub profile icon hidden temporarily

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import './Footer.css';
 import { IconButton } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
