59bf6e17f651dfd65778f33d37105961b9da71c7
Abhishek Soni <abhishek1600soni@gmail.com>
2023-05-31 16:50:48 +0530

    Updated code to handle error(s) in some cases

--- src/Components/Whois/Whois.js
+++ src/Components/Whois/Whois.js
@@ -39,7 +39,6 @@ const Whois = (props) => {
         }
     }, [props.url, apiUrl, options]);
 
-
     const rows = result === null ? [] : [
         { label: 'Domain Name', value: result.name },
         // Checking if there are more than one values in result.status
@@ -47,27 +46,30 @@ const Whois = (props) => {
             label: 'Domain Status', value: Array.isArray(result.status) && result.status.length > 1 ? Object.entries(result.status).map((status) => {
                 let [key, value] = status;
                 return <span key={key}>{value}<br /></span>;
-            }) : result.status
+            }) : result.status || 'Not Available'
         },
         {
-            label: 'Nameservers', value: Object.entries(result.nameserver).map((ns) => {
-                let [key, value] = ns;
-                return <span key={key}>{value}<br /></span>;
-            })
+            label: 'Nameservers',
+            value: result.nameserver ?
+                Object.entries(result.nameserver).map((ns) => {
+                    let [key, value] = ns;
+                    return <span key={key}>{value}<br /></span>;
+                }) :
+                'Not Available'
         },
         { label: 'IP Address', value: result.ips },
-        { label: 'Creation Date:', value: result.created },
-        { label: 'Updated Date', value: result.changed },
-        { label: 'Whois Server', value: result.whoisserver },
-        { label: 'Registry Expiry Date', value: result.expires },
+        { label: 'Creation Date:', value: result.created || 'Not Available' },
+        { label: 'Updated Date', value: result.changed || 'Not Available' },
+        { label: 'Whois Server', value: result.whoisserver || 'Not Available' },
+        { label: 'Registry Expiry Date', value: result.expires || 'Not Available' },
         { label: 'Registered', value: result.registered ? 'Yes' : 'No' },
         { label: 'DNSSEC', value: typeof result.dnssec === 'boolean' ? (result.dnssec ? 'Yes' : 'No') : result.dnssec },
-        { label: 'Registrar Name', value: result.registrar.name },
-        { label: 'Registrar ID', value: result.registrar.id || 'Not Available' },
-        { label: 'Registrar Email', value: result.registrar.email || 'Not Available' },
+        { label: 'Registrar Name', value: (result.registered && result.registrar.name) || 'Not Available' },
+        { label: 'Registrar ID', value: (result.registered && result.registrar.id) || 'Not Available' },
+        { label: 'Registrar Email', value: (result.registered && result.registrar.email) || 'Not Available' },
         // { label: 'Registrar Email', value: result.registrar.email.length > 1 ? result.registrar.email.map((email, index) => { return <span key={index}>{email}<br /></span> }) || 'Not Available': result.registrar.email || 'Not Available' },
-        { label: 'Registrar URL', value: result.registrar.url || 'Not Available' },
-        { label: 'Registrar Phone', value: result.registrar.phone || 'Not Available' }
+        { label: 'Registrar URL', value: (result.registered && result.registrar.url) || 'Not Available' },
+        { label: 'Registrar Phone', value: (result.registered && result.registrar.phone) || 'Not Available' }
         // { label: 'Registrar Phone', value:result.registrar.phone.length>1? result.registrar.phone.map((phone, index) => { return <span key={index}>{phone}<br /></span> }) || 'Not Available': result.registrar.phone || 'Not Available' },
     ];
 
